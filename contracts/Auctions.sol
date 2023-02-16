2a9c38b3be2b79768bed4f0b2857f1e00169c5b1
FarzeenKist <farzeenkistnareddy@gmail.com>
2023-02-17 00:45:13 +0400

    Bug fix in EndAucByOwner and restructure comments for functions

--- contracts/Auctions.sol
+++ contracts/Auctions.sol
@@ -56,6 +56,14 @@ contract AuctionNFTs is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
         _;
     }
 
+    modifier validAuction(uint256 tokenId) {
+        require(
+            AllAucs[tokenId].tokenId == tokenId,
+            "Invalid Token, Not Part Of The Auction"
+        );
+        _;
+    }
+
     /**
      * @dev to mint a new token, a mint fee is paid(if any)
      * @param uri URL containing the metadata for the token
@@ -83,19 +91,6 @@ contract AuctionNFTs is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     }
 
     /**
-     * @return Array returns a fixed array containing every tokenIds associated with the caller;
-     */
-    function OwnWallet() public view returns (uint256[] memory) {
-        uint256 ownerTokenCount = balanceOf(msg.sender);
-        uint256[] memory tokenIds = new uint256[](ownerTokenCount);
-        // iterates upto the current balance of caller to retrieve all tokenIds in the _ownedTokens array
-        for (uint256 i = 0; i < ownerTokenCount; i++) {
-            tokenIds[i] = tokenOfOwnerByIndex(msg.sender, i);
-        }
-        return tokenIds;
-    }
-
-    /**
      * @dev reverts if token is already in an auction
      * @notice allow tokens' owners to auction their NFTs
      */
@@ -104,6 +99,9 @@ contract AuctionNFTs is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
             AllAucs[tokenId].tokenId != tokenId,
             "Token Has Already Been Added To The Auction Or Has Been Auctioned"
         );
+
+        _ActAucCount.increment(); // Here We Are Updating This State Variables By increamenting Their Counts
+        _AucIdCount.increment();
         Auction storage B = AllAucs[tokenId];
         //In the next 7 lines we are creating a new struct and  updating its properties and  variables.
         B.tokenId = tokenId;
@@ -113,20 +111,20 @@ contract AuctionNFTs is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
         B.aucOver = false;
         B.toPay = false;
         B.aucIdx = _AucIdCount.current();
-        _ActAucCount.increment(); // Here we are Updating This State Variable By incrementing their counts
-        _AucIdCount.increment();
     }
 
-    // Making This Call Will Add The Callers Bid To The Requested Tokens Struct For Bidders.
-    function BidOnAuc(uint256 tokenId, uint256 BId) public {
+    /**
+     * @dev If caller has already bid in the auction, the bid's value is updated and then compared to the current top bid
+     * @notice allow users to make a bid in an auction
+     */
+    function BidOnAuc(uint256 tokenId, uint256 BId)
+        public
+        validAuction(tokenId)
+    {
         require(
             ownerOf(tokenId) != msg.sender,
             "You Cant bid on your own token"
         );
-        require(
-            AllAucs[tokenId].tokenId == tokenId,
-            "Invalid Token, Not Part Of The Auction"
-        ); // These Make's Some Required Validations In the Function
         require(!AllAucs[tokenId].aucOver, "Auction On This Token Is Over"); // To Be Asserted Before Addiing The Bidder To The Called Token's Auction
         if (AucBidderIdx[tokenId][msg.sender].adr == msg.sender) {
             // Here A condition Is Met if The Bidder Already Exists
@@ -137,19 +135,20 @@ contract AuctionNFTs is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
             }
             return;
         }
-        // potential bug here
-        uint256 new_length = AllAucs[tokenId].biddersCount + 1;
-        AucBidder storage B = AucBidderIdx[tokenId][msg.sender]; // These Next Few Lines  We Will Be Adding A new Bidder If It Dose'nt exists
-        B.adr = msg.sender; // And Updates Tokens Auction And Bidders Struct Properties And Variable.
-        B.bid = BId; // If Necessarry.
-        B.bidderIdx = new_length;
         if (AllAucs[tokenId].topAucBid < BId) {
+            AllAucs[tokenId].biddersCount++;
+            AucBidder storage B = AucBidderIdx[tokenId][msg.sender]; // These Next Few Lines  We Will Be Adding A new Bidder If It Dose'nt exists
+            B.adr = msg.sender; // And Updates Tokens Auction And Bidders Struct Properties And Variable.
+            B.bid = BId;
+            B.bidderIdx = AllAucs[tokenId].biddersCount;
             AllAucs[tokenId].topAucBid = BId;
             AllAucs[tokenId].topAucBidder = msg.sender;
         }
     }
 
-    // Making This Call Will Explicitly Change And Update's The Uri To A Token By The Caller.
+    /**
+     * @notice allows token owners to update the URI of their NFTs
+     */
     function _setURI(uint256 tokenId, string memory uri)
         public
         onlyTokenOwner(tokenId)
@@ -158,11 +157,11 @@ contract AuctionNFTs is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     }
 
     // Making This Call Is Done By The Owner Of A Token That Was On AUction WIthout A Bid And Ended to Restore It Back On Auctions
-    function ReactivateDeadAuc(uint256 tokenId) public onlyTokenOwner(tokenId) {
-        require(
-            AllAucs[tokenId].tokenId == tokenId,
-            "Token Has not Been Added To The Auction"
-        ); //Validations In the Function To Be Asserted Before Restoring The Auction
+    function ReactivateDeadAuc(uint256 tokenId)
+        public
+        onlyTokenOwner(tokenId)
+        validAuction(tokenId)
+    {
         require(AllAucs[tokenId].aucOver, "This Token Is Still On Auction");
         require(
             AllAucs[tokenId].topAucBid <= 0,
@@ -173,21 +172,61 @@ contract AuctionNFTs is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
         _ActAucCount.increment();
     }
 
-    // Making This Call During and After The Auction IS Over. Get's The HighestBidder And HighestBid On The Token Called.
-    function AucInfo(uint256 tokenId) public view returns (uint256, address) {
+    /**
+     * @dev Highest bidder is approved to be able to transfer the token
+     * @notice allows auctions' owners to end their auctions
+     */
+    function EndAucByOwner(uint256 tokenId)
+        public
+        onlyTokenOwner(tokenId)
+        validAuction(tokenId)
+    {
         require(
-            AllAucs[tokenId].tokenId == tokenId,
-            "Invalid Token, Not Part Of The Auction"
-        ); // This Make's A Required Validations In the Function
-        return (AllAucs[tokenId].topAucBid, AllAucs[tokenId].topAucBidder); // Returned Value
+            !AllAucs[tokenId].aucOver,
+            "Auction Has To Be Active To Be Ended"
+        );
+        AllAucs[tokenId].aucOver = true; //This Line And All That Followa Will Be  Updating The Necessary Properties, Variables And Called functions
+        _transfer(msg.sender, address(this), tokenId);
     }
 
-    //Making This Call Will Return's An Array Of Active Tokens On Auction.
+    /**
+        * @dev Payment is made to auction's owner and NFT is transferred to the highest bidder
+        * @notice allows the highest bidder of an auction to claim and pay for their bought items
+     */
+    function TransferAucToWinner(uint256 tokenId) public payable {
+        require(
+            msg.sender == AuctionTopBidder(tokenId),
+            "Invalid Address, Caller IS Not The Hightest BIdder"
+        ); // This And The Next Line Will Be Adding Some Required Assertions To the Functions
+        require(
+            msg.value == AllAucs[tokenId].topAucBid,
+            "Invalid Payment, Bid Is Does Not Equal Hightest Bid"
+        );
+        require(
+            AllAucs[tokenId].toPay == false,
+            "Auctioned NFT has already been transferred"
+        );
+        AllAucs[tokenId].toPay = true;
+        _transfer(address(this), msg.sender, tokenId);
+        (bool success, ) = payable(AllAucs[tokenId].aucOwner).call{
+            value: msg.value
+        }("");
+        require(success, "Transfer failed");
+    }
+
+    //This Call Is Made Only By Contract Owner To Set The Minting Cost.
+    function setCost(uint256 _newCost) public onlyOwner {
+        MINT_PRICE = _newCost;
+    }
+
+        /**
+        * @return Array returns an array of auctionIds for auctions that haven't yet ended
+     */
     function ActiveAucs() public view returns (uint256[] memory) {
         uint256[] memory ActBids = new uint256[](_ActAucCount.current());
         uint256 auctionCounts = _AucIdCount.current();
         uint256 index = 0;
-        for (uint256 i = 1; i < auctionCounts; i++) {
+        for (uint256 i = 1; i <= auctionCounts; i++) {
             if (AllAucs[i].aucIdx == i && AllAucs[i].aucOver == false) {
                 // Here We Will Be Using A loop To lookup The Available Active Auctions
                 ActBids[index] = AllAucs[i].tokenId; // To Complete The Process i.e Return The Array.
@@ -198,19 +237,17 @@ contract AuctionNFTs is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
         return ActBids;
     }
 
-    //Making This Call Will Return's An Array Of Ended Tokens On Auction.
+    /**
+        * @return Array returns an array of auctionIds for auctions that have ended
+     */
     function EndedAucs() public view returns (uint256[] memory) {
         uint256[] memory Ended = new uint256[](
             _AucIdCount.current() - _ActAucCount.current()
         );
         uint256 index = 0;
         uint256 auctionCounts = _AucIdCount.current();
-        for (uint256 i = 1; i < auctionCounts; i++) {
-            if (
-                AllAucs[i].aucIdx == i &&
-                AllAucs[i].aucOver == true &&
-                AllAucs[i].toPay == true
-            ) {
+        for (uint256 i = 1; i <= auctionCounts; i++) {
+            if (AllAucs[i].aucIdx == i && AllAucs[i].aucOver == true) {
                 // Here We Will Be Using A loop To lookup The Available Ended Auctions
                 Ended[index] = AllAucs[i].tokenId; // To Complete The Process i.e Return The Array.
                 index++;
@@ -219,62 +256,29 @@ contract AuctionNFTs is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
         return Ended;
     }
 
-    //Making This Call Returns The Requested Tokens Highest Bidder's Address
-    function AuctionTopBidder(uint256 tokenId) public view returns (address) {
-        require(
-            AllAucs[tokenId].tokenId == tokenId,
-            "Invalid Token, Not Part Of The Auction"
-        ); // This Make's A Required Validations In the Function
+    /**
+        * @return Address returns the address of the highest bidder for an auction
+     */
+    function AuctionTopBidder(uint256 tokenId)
+        public
+        view
+        validAuction(tokenId)
+        returns (address)
+    {
         return AllAucs[tokenId].topAucBidder;
     }
 
-    // This Call Is Made By The Token Owner When Autions On That Token IS Over
-    // And Enables The Highest Bidder To Pay for His Token
-    function EndAucByOwner(uint256 tokenId) public onlyTokenOwner(tokenId) {
-        require(
-            AllAucs[tokenId].tokenId == tokenId,
-            "Token Is Not Part Of The Auction"
-        ); // Before The AUction Is Ended And The And ENable Te Transfer Process
-        require(
-            !AllAucs[tokenId].aucOver,
-            "Auction Has To Be Active To Be Ended"
-        );
-        AllAucs[tokenId].aucOver = true; //This Line And All That Followa Will Be  Updating The Necessary Properties, Variables And Called functions
-        AllAucs[tokenId].toPay = true; // Needed To Complete this Proccess.
-        _ActAucCount.decrement();
-        approve(AllAucs[tokenId].topAucBidder, tokenId);
-    }
-
-    // This call Is To Varify The Possiblities Of Payment And Transfer Of Token In The Call
-    function ToPay(uint256 tokenId) public view returns (bool) {
-        require(
-            msg.sender == AllAucs[tokenId].topAucBidder,
-            "Requires the highest Bidder as the sender"
-        ); // This Make's A Required Validations In the Function
-        return AllAucs[tokenId].toPay; // Returned Value
-    }
-
-    //This Is called WHen The Aution Winner Has the Token In Collectables, To Pay And to Collect The Token.
-    function TransferAucToWinner(address payable item_owner, uint256 tokenId)
-        public
-        payable
-    {
-        require(
-            msg.sender == AuctionTopBidder(tokenId),
-            "Invalid Address, Caller IS Not The Hightest BIdder"
-        ); // This And The Next Line Will Be Adding Some Required Assertions To the Functions
-        require(
-            msg.value == AllAucs[tokenId].topAucBid,
-            "Invalid Payment, Bid Is Does Not Equal Hightest Bid"
-        );
-        safeTransferFrom(item_owner, msg.sender, tokenId);
-        (bool success, ) = payable(item_owner).call{value: msg.value}("");
-        require(success, "Transfer failed");
-    }
-
-    //This Call Is Made Only By Contract Owner To Set The Minting Cost.
-    function setCost(uint256 _newCost) public onlyOwner {
-        MINT_PRICE = _newCost;
+    /**
+     * @return Array returns a fixed array containing every tokenIds associated with the caller;
+     */
+    function OwnWallet() public view returns (uint256[] memory) {
+        uint256 ownerTokenCount = balanceOf(msg.sender);
+        uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+        // iterates upto the current balance of caller to retrieve all tokenIds in the _ownedTokens array
+        for (uint256 i = 0; i < ownerTokenCount; i++) {
+            tokenIds[i] = tokenOfOwnerByIndex(msg.sender, i);
+        }
+        return tokenIds;
     }
 
     // The following functions are overrides required by Solidity.
