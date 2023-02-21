2761253fd856c0537196a281265f90b57a3c0e7f
federico suarez <fedesuarez16@gmail.com>
2023-02-21 10:27:05 -0300

    init

--- contracts/Auctions.sol
+++ contracts/Auctions.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.7.0 <0.9.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol"
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 // #tag The Cockpit
